3941227d9826ec850a18e3a6a0106180a74f4e75
bijoymamud <bijoymamud.09@gmail.com>
2024-05-02 15:48:21 +0600

    forLoop

---
+++ doWhileLoop.php
@@ -0,0 +1,34 @@
+<?php
+
+$i = 1;
+
+do {
+  echo $i ."<br>";
+  $i++;
+} while ($i <= 10);
+
+
+//multiplication
+
+$x = 5;
+$y = 1;
+
+do {
+  echo "5 x {$y} = {$x} <br>";
+  $y++;
+  $x += 5;
+} while ($x <= 50);
+
+
+$a = 12;
+$b = 1;
+
+do {
+  echo "12 x {$b} = {$a} <br>";
+  $b++;
+  $a+=12;
+  
+} while ($a <= 240);
+
+
+?>
--- whileLoop.php
+++ whileLoop.php
@@ -5,10 +5,21 @@ $into = 1;
 
 while ($twenty <= 200) {
  
-  echo "20 x {$into} = {$twenty} .<br>";
+  echo "20 x {$into} = {$twenty} <br>";
   $into++;
   $twenty += 20;
 
+}
+
+
+//assignment from thapa technical
+$multy = 12;
+$plication = 1;
+
+while ($multy <= 240) {
+  echo "12 x {$plication} = {$multy} <br>";
+  $plication++;
+  $multy+= 12;
 }
 
 
